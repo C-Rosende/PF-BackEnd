9b067982a58f500329ada86984fa243e450110fc
Clemente Rosende <cleme.rosende@gmail.com>
2024-03-19 23:53:33 -0300

    Entrega 2.

--- productManager.js
+++ productManager.js
@@ -7,12 +7,12 @@ class ProductManager {
 
     addProduct(title, description, price, thumbnail, code, stock) {
         if (!title || !description || !price || !thumbnail || !code || !stock) {
-            console.error('All fields are required');
+            console.error('Todos los campos son obligatorios');
             return;
         }
 
         if (this.products.some(product => product.code === code)) {
-            console.error('Product code must be unique');
+            console.error('El código del producto debe ser único');
             return;
         }
 
@@ -35,11 +35,36 @@ class ProductManager {
         const product = this.products.find(product => product.id === id);
 
         if (!product) {
-            console.error('Not found');
+            console.error('Producto no encontrado');
             return;
         }
 
         return product;
     }
+
+    updateProduct(id, newProductData) {
+        const product = this.products.find(product => product.id === id);
+
+        if (!product) {
+            console.error('Producto no encontrado');
+            return;
+        }
+
+        delete newProductData.id;
+
+        Object.assign(product, newProductData);
+    }
+
+    deleteProduct(id) {
+        const productIndex = this.products.findIndex(product => product.id === id);
+
+        if (productIndex === -1) {
+            console.error('Producto no encontrado');
+            return;
+        }
+
+        this.products.splice(productIndex, 1);
+    }
 }
+
 module.exports = ProductManager;
